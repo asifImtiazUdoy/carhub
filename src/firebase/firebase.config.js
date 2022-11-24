37e1012f998d09292475cf0379dadbef721efa71
asif-imtiaz-udoy <asifimtiaz240@gmail.com>
2022-11-24 14:19:48 +0600

    firebase install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXTYFsaNgiUV4hcq-zIWVxxS1YvMajMcg",
+  authDomain: "carhub-6764b.firebaseapp.com",
+  projectId: "carhub-6764b",
+  storageBucket: "carhub-6764b.appspot.com",
+  messagingSenderId: "775772857415",
+  appId: "1:775772857415:web:2ba5ad0736aff70f0a3d62"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
